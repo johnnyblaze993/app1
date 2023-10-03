78f32141de008d0e62ea4d5d8a4d43db8cfaf5ef
alvarezjo <jonathan.alvarez@axientcorp.com>
2023-10-03 11:46:05 -0500

    app one changes

--- src/main/java/com/example/App1Controller.java
+++ src/main/java/com/example/App1Controller.java
@@ -2,23 +2,28 @@ package com.example;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.HttpRequest;
 import jakarta.inject.Inject;
 import io.micronaut.http.client.HttpClient; 
+import io.micronaut.http.MediaType;
 
-@Controller("/App1")
+@Controller("/hello") // <1>
 public class App1Controller {
 
-    @Inject
+        @Inject
     private HttpClient httpClient;
 
-    @Get("/")
+
+    @Get // <2>
+    @Produces(MediaType.TEXT_PLAIN) // <3>
     public String index() {
-        return "Hello from App1!";
+        return "Hello World"; // <4>
     }
-    @Get("/callApp2")
+
+        @Get("/callApp2")
     public String callApp2() {
-        HttpRequest<String> request = HttpRequest.GET("http://localhost:8080/App2");
+        HttpRequest<String> request = HttpRequest.GET("http://localhost:9090/app2/hello");
         String body = httpClient.toBlocking().retrieve(request);
         return body;
     }
